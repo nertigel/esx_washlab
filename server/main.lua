aef54c5ef75b880f1d49df9b6bd5706784d67ed6
Patrik Nertigel <nertigel14475@gmail.com>
2021-01-05 17:59:19 +0200

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -5,6 +5,7 @@
 
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local storedAccounts = {}
 
 ESX.RegisterServerCallback('esx_washlab:callback:getBlackMoneyAmount', function(source, cb)
     local sourcePlayer = ESX.GetPlayerFromId(source)
@@ -16,20 +17,40 @@ ESX.RegisterServerCallback('esx_washlab:callback:getBlackMoneyAmount', function(
 end)
 
 RegisterNetEvent('esx_washlab:server:removeBlackMoney')
-AddEventHandler('esx_washlab:server:removeBlackMoney', function(amount)
-    local sourcePlayer = ESX.GetPlayerFromId(source)
+AddEventHandler('esx_washlab:server:removeBlackMoney', function()
+    local _source = source
+    local sourcePlayer = ESX.GetPlayerFromId(_source)
+    local amount = sourcePlayer.getAccount('black_money').money
     
-    sourcePlayer.removeAccountMoney('black_money', amount)
+    if (amount ~= nil and amount > 0) then
+        sourcePlayer.removeAccountMoney('black_money', amount)
+        if (storedAccounts[_source] ~= nil and storedAccounts[_source] > 0) then
+            storedAccounts[_source] = storedAccounts[_source] + amount
+        else
+            storedAccounts[_source] = amount
+        end
+    end
 end)
 
 RegisterNetEvent('esx_washlab:server:giveCleanMoney')
 AddEventHandler('esx_washlab:server:giveCleanMoney', function(amount)
+    local _source = source
     local sourcePlayer = ESX.GetPlayerFromId(source)
-    if Config.TakePercentage then
-        total = amount * Config.WashPercentage        
-    else
-        total = amount
+    local total = amount
+    if (Config.TakePercentage) then
+        total = amount * Config.WashPercentage
     end
-    sourcePlayer.addMoney(total) 
-    TriggerClientEvent('esx:showNotification', source, 'You washed ~y~€' .. total .. '~s~.')
+
+    --[[
+        "protected" part
+    ]]
+    if (storedAccounts[_source] ~= nil and storedAccounts[_source] > 0) then
+        if (storedAccounts[_source] >= total) then
+            sourcePlayer.addMoney(total) 
+            TriggerClientEvent('esx:showNotification', _source, 'You washed ~y~$' .. total .. '~s~.')
+            storedAccounts[_source] = storedAccounts[_source] - total
+        else
+            TriggerClientEvent('esx:showNotification', _source, 'dont exploit please - nertigel')
+        end
+    end
 end)
